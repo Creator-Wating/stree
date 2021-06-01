25908239f6a49a2aa79b7329053e8f77f1af700a
Creator-Wating <815251620@qq.com>
2021-06-01 21:53:15 +0800

    commplet the second step

---
+++ id.c
@@ -0,0 +1,17 @@
+#include <dirent.h> 
+#include <stdio.h> 
+#include <ctype.h>
+
+int main(void) {
+  DIR *d;
+  struct dirent *dir;
+  d = opendir("/mnt/d/pstree/os-workbench");
+  if (d != NULL) {
+    while ((dir = readdir(d)) != NULL ) {
+        if (opendir (dir->d_name) != NULL && isdigit(*dir->d_name) )
+      printf("%s\n", dir->d_name);
+    }
+  }
+  closedir(d);
+  return(0);
+}
